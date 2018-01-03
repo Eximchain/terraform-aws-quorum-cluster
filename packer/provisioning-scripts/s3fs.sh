46f785c400dd013a142525807b5d704ae76d76c3
Louis <4107447+Lsquared13@users.noreply.github.com>
2018-01-02 20:13:48 -0500

    Use specific release of s3fs

--- packer/provisioning-scripts/s3fs.sh
+++ packer/provisioning-scripts/s3fs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
 
+RELEASE=v1.83
+
 git clone https://github.com/s3fs-fuse/s3fs-fuse.git
 pushd s3fs-fuse >/dev/null
+git checkout tags/$RELEASE
 ./autogen.sh
 ./configure
 make
