8bfcd31980ccdab8a4156b0f896dec00a3ad6b13
Louis <4107447+Lsquared13@users.noreply.github.com>
2018-01-11 13:19:15 -0500

    Quorum nodes output DNS instead of IP

--- terraform/modules/quorum-cluster/outputs.tf
+++ terraform/modules/quorum-cluster/outputs.tf
@@ -1,17 +1,17 @@
-output "quorum_maker_node_ips" {
-  value = "${aws_instance.quorum_maker_node.*.public_ip}"
+output "quorum_maker_node_dns" {
+  value = "${aws_instance.quorum_maker_node.*.public_dns}"
 }
 
-output "quorum_validator_node_ips" {
-  value = "${aws_instance.quorum_validator_node.*.public_ip}"
+output "quorum_validator_node_dns" {
+  value = "${aws_instance.quorum_validator_node.*.public_dns}"
 }
 
-output "quorum_observer_node_ips" {
-  value = "${aws_instance.quorum_observer_node.*.public_ip}"
+output "quorum_observer_node_dns" {
+  value = "${aws_instance.quorum_observer_node.*.public_dns}"
 }
 
-output "bootnode_ips" {
-  value = "${aws_instance.bootnode.*.public_ip}"
+output "bootnode_dns" {
+  value = "${aws_instance.bootnode.*.public_dns}"
 }
 
 output "vault_server_ips" {
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,17 +1,17 @@
-output "quorum_maker_node_ips" {
-  value = "${module.quorum_cluster.quorum_maker_node_ips}"
+output "quorum_maker_node_dns" {
+  value = "${module.quorum_cluster.quorum_maker_node_dns}"
 }
 
-output "quorum_validator_node_ips" {
-  value = "${module.quorum_cluster.quorum_validator_node_ips}"
+output "quorum_validator_node_dns" {
+  value = "${module.quorum_cluster.quorum_validator_node_dns}"
 }
 
-output "quorum_observer_node_ips" {
-  value = "${module.quorum_cluster.quorum_observer_node_ips}"
+output "quorum_observer_node_dns" {
+  value = "${module.quorum_cluster.quorum_observer_node_dns}"
 }
 
-output "bootnode_ips" {
-  value = "${module.quorum_cluster.bootnode_ips}"
+output "bootnode_dns" {
+  value = "${module.quorum_cluster.bootnode_dns}"
 }
 
 output "vault_server_ips" {
