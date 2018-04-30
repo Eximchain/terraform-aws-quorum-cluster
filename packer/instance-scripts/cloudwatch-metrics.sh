bb0b32a80956808b48dd7f32f496750cc11b8162
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-04-30 11:44:59 -0400

    Extract JQ expression into variable

--- packer/instance-scripts/cloudwatch-metrics.sh
+++ packer/instance-scripts/cloudwatch-metrics.sh
@@ -10,13 +10,19 @@ readonly NETWORK_ID=$(cat /opt/quorum/info/network-id.txt)
 readonly RPC_ADDR=$1
 readonly RPC_PORT=$2
 
-function emit_pending_transactions_metric {
-  local readonly METRIC="PendingQuorumTransactions"
-  local readonly METHOD="txpool_status"
+function emit_metric {
+  local readonly METRIC=$1
+  local readonly METHOD=$2
 
   RESPONSE_JSON=$(curl -X POST --data "{\"jsonrpc\":\"2.0\",\"method\":\"$METHOD\",\"params\":[],\"id\":1}" $RPC_ADDR:$RPC_PORT)
   PENDING=$(printf "%d" $(echo $RESPONSE_JSON | jq -r .result.pending))
   aws cloudwatch put-metric-data --region $PRIMARY_REGION --namespace $NAMESPACE --metric-name $METRIC --value $PENDING --dimensions NetworkID=$NETWORK_ID
+}
+
+function emit_pending_transactions_metric {
+  local readonly METRIC="PendingQuorumTransactions"
+  local readonly METHOD="txpool_status"
+  emit_metric $METRIC $METHOD
 }
 
 while true
