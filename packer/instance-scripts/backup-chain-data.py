8e9506fe22f51c20a6b59bccc638576d1627670c
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-06-01 16:38:26 -0400

    backup-chain-data can store a separate backup per block

--- packer/instance-scripts/backup-chain-data.py
+++ packer/instance-scripts/backup-chain-data.py
@@ -2,7 +2,10 @@ import argparse
 import boto3
 import os.path
 import sys
+import urllib2
 import xmlrpclib
+
+from ethjsonrpc import EthJsonRpc
 
 # max size in bytes before uploading in parts. between 1 and 5 GB recommended
 MAX_SIZE = 50 * 1000 * 1000
@@ -13,19 +16,24 @@ PART_SIZE = 6 * 1000 * 1000
 CHAIN_DATA_DIR = '/home/ubuntu/.ethereum/geth/chaindata/'
 
 GETH_SUPERVISOR_PROCESS = 'quorum'
+GETH_PORT = 22000
 
 BACKUP_BUCKET_FILE = '/opt/quorum/info/data-backup-bucket.txt'
 with open(BACKUP_BUCKET_FILE, 'r') as f:
     BACKUP_BUCKET = f.read().strip()
 
+hostname = urllib2.urlopen("http://169.254.169.254/latest/meta-data/public-hostname").read()
+
 supervisor_client = xmlrpclib.Server('http://localhost:9001/RPC2').supervisor
 s3 = boto3.resource('s3')
+eth_client = EthJsonRpc(hostname, GETH_PORT)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Back up and restore chain data')
-    action = parser.add_mutually_exclusive_group(required=True)
-    action.add_argument('--backup', action='store_true', help='Back up data from this node')
-    action.add_argument('--restore', action='store_true', help='Restore data from s3')
+    subparsers = parser.add_subparsers(dest='command', help='Command to execute')
+    parser_backup = subparsers.add_parser('backup', help='Back up data from this node')
+    parser_restore = subparsers.add_parser('restore', help='Restore data from s3')
+    parser_restore.add_argument('--block', required=True, type=int, dest='block_to_restore')
     return parser.parse_args()
 
 def pause_geth():
@@ -81,6 +89,9 @@ def s3_download(bucket_name, source_dir, dest_dir):
     bucket = s3.Bucket(bucket_name)
     objects = bucket.objects.all()
     objects_to_copy = filter(lambda obj: obj.key.startswith(source_dir), objects)
+    if not objects_to_copy:
+        print "No objects found to download"
+        return
     for object_summary in objects_to_copy:
         object = object_summary.Object()
         key_without_prefix = object.key.split('/')[-1]
@@ -88,21 +99,25 @@ def s3_download(bucket_name, source_dir, dest_dir):
         print 'Downloading %s from Amazon S3 bucket %s' % (destpath, bucket_name)
         object.download_file(destpath)
 
-def backup_chain_data():
-    dest_dir = ''
+def backup_chain_data(block_number):
+    dest_dir = 'block-%s/' % (block_number)
+    print 'Backing up chain at block %s' % (block_number)
     s3_upload(BACKUP_BUCKET, CHAIN_DATA_DIR, dest_dir)
 
-def restore_chain_data():
-    source_dir = ''
+def restore_chain_data(block_number):
+    source_dir = 'block-%s/' % (block_number)
+    print 'Restoring chain from block %s' % (block_number)
     s3_download(BACKUP_BUCKET, source_dir, CHAIN_DATA_DIR)
 
 args = parse_args()
 
-pause_geth()
 try:
-    if args.backup:
-        backup_chain_data()
-    elif args.restore:
-        restore_chain_data()
+    if args.command == 'backup':
+        current_block = eth_client.eth_blockNumber()
+        pause_geth()
+        backup_chain_data(current_block)
+    elif args.command == 'restore':
+        pause_geth()
+        restore_chain_data(args.block_to_restore)
 finally:
     resume_geth()
