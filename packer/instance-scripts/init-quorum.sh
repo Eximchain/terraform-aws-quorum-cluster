1d423a1fe7d778566e0dfedfa8c87f89cdc6937f
John O'Sullivan <john@eximchain.com>
2018-06-29 12:09:50 -0400

    Dropped unused PW_FILE variable

--- packer/instance-scripts/init-quorum.sh
+++ packer/instance-scripts/init-quorum.sh
@@ -27,7 +27,6 @@ function generate_quorum_supervisor_config {
     local THIS_REGION=$(cat /opt/quorum/info/aws-region.txt)
 
     local VERBOSITY=2
-    local PW_FILE="/tmp/geth-pw"
     local GLOBAL_ARGS="--networkid $NETID --rpc --rpcaddr $HOSTNAME --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --rpcport 22000 --rpccorsdomain \"*\" --port 21000 --verbosity $VERBOSITY --jitvm=false --privateconfigpath $CONSTELLATION_CONFIG"
 
     # Assemble list of bootnodes
