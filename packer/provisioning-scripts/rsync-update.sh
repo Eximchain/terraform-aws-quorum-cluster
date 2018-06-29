751ab0170986cbde68028e6e1ba7c4fb7ca3e3c3
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-06-29 15:56:12 -0400

    Build rsync 3.1.3 from source during packer build
    Fix for vulnerability https://nvd.nist.gov/vuln/detail/CVE-2017-15994

---
+++ packer/provisioning-scripts/rsync-update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu -o pipefail
+
+VER=3.1.3
+DIR_NAME=rsync-$VER
+
+wget https://download.samba.org/pub/rsync/src/$DIR_NAME.tar.gz
+tar -xzf $DIR_NAME.tar.gz
+cd $DIR_NAME
+./configure
+make
+sudo cp ./rsync /usr/bin/
+cd .. && rm -rf $DIR_NAME.tar.gz $DIR_NAME
