3aebb9d7bb300ffcd8a4c5ad52ac8de7c5b33036
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-07-06 13:47:32 -0400

    Upgrade binutils to version 2.30
    Addresses Issue #20

---
+++ packer/provisioning-scripts/binutils-update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu -o pipefail
+
+VER=2.30
+DIR_NAME=binutils-$VER
+
+wget http://ftp.gnu.org/gnu/binutils/$DIR_NAME.tar.gz
+tar -xzf $DIR_NAME.tar.gz
+cd $DIR_NAME
+./configure
+make
+sudo make install
+cd .. && rm -rf $DIR_NAME.tar.gz $DIR_NAME
