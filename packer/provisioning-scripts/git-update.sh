6fbe86ffd2aca19eaa4324bfb52e093fcca915ae
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-07-11 12:26:31 -0400

    Build git v2.18.0 during packer builds
    Fixes security issue. Partially addresses Issue #20 .

---
+++ packer/provisioning-scripts/git-update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu -o pipefail
+
+RELEASE="v2.18.0"
+
+git clone https://github.com/git/git.git
+pushd git >/dev/null
+git checkout $RELEASE
+make
+sudo make install
+popd >/dev/null
