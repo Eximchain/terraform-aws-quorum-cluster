f7a0a3955664c134074c9cc5877fb429167a3d20
John O'Sullivan <john@eximchain.com>
2018-08-30 11:26:09 -0400

    Added owners arg for generate-quorum-config

--- packer/provisioning-scripts/quorum-genesis.sh
+++ packer/provisioning-scripts/quorum-genesis.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
 
-VERSION=v1.5.2
+# TODO: Reset address to Exim fork, checkout release
+# VERSION=v1.5.2
 
-git clone https://github.com/Eximchain/quorum-genesis.git /opt/quorum/lib/quorum-genesis
+git clone https://github.com/john-osullivan/exim-genesis.git /opt/quorum/lib/quorum-genesis
+# git clone https://github.com/Eximchain/quorum-genesis.git /opt/quorum/lib/quorum-genesis
 cd /opt/quorum/lib/quorum-genesis
-git checkout tags/$VERSION
+# git checkout tags/$VERSION
 sudo npm install -g -y
--- packer/provisioning-scripts/quorum.sh
+++ packer/provisioning-scripts/quorum.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
 
-RELEASE="v1.2.1-modified"
+# RELEASE="v1.2.1-modified"
+RELEASE="weylGov"
 
 source /etc/profile.d/quorum-custom.sh
-git clone https://github.com/Eximchain/quorum.git
+# git clone https://github.com/Eximchain/quorum.git
+git clone https://github.com/john-osullivan/quorum.git
 pushd quorum >/dev/null
 git checkout $RELEASE
 make all
