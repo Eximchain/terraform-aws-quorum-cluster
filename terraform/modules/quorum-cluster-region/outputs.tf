baee8a81e65d0eab42468ac207209b23d23f0279
chuacw <40189902+EximChua@users.noreply.github.com>
2018-11-29 15:10:18 +0800

    Update output attribute
    Update output attribute

--- terraform/modules/quorum-cluster-region/outputs.tf
+++ terraform/modules/quorum-cluster-region/outputs.tf
@@ -3,15 +3,15 @@ output "quorum_vpc_id" {
 }
 
 output "quorum_maker_node_dns" {
-  value = "${data.aws_instances.quorum_maker_node.*.public_dns}"
+  value = "${data.aws_instances.quorum_maker_node.*.public_ips}"
 }
 
 output "quorum_validator_node_dns" {
-  value = "${data.aws_instances.quorum_validator_node.*.public_dns}"
+  value = "${data.aws_instances.quorum_validator_node.*.public_ips}"
 }
 
 output "quorum_observer_node_ips" {
-  value = "${coalescelist(aws_eip.quorum_observer.*.public_ip, data.aws_instances.quorum_observer_node.*.public_ip)}"
+  value = "${coalescelist(aws_eip.quorum_observer.*.public_ip, data.aws_instances.quorum_observer_node.*.public_ips)}"
 }
 
 output "bootnode_ips" {
