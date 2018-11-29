230220275a84b35c08e071c7f98b5b7753699e53
chuacw <40189902+EximChua@users.noreply.github.com>
2018-11-29 15:30:56 +0800

    Fix validator and observer output
    Fix validator and observer output

--- terraform/modules/quorum-cluster-region/outputs.tf
+++ terraform/modules/quorum-cluster-region/outputs.tf
@@ -7,11 +7,11 @@ output "quorum_maker_node_dns" {
 }
 
 output "quorum_validator_node_dns" {
-  value = "${data.aws_instance.quorum_validator_node.*.public_dns}"
+  value = "${length(data.aws_instance.quorum_validator_node.*.public_dns) != 0 ? data.aws_instance.quorum_validator_node.*.public_dns: ""}"
 }
 
 output "quorum_observer_node_ips" {
-  value = "${coalescelist(aws_eip.quorum_observer.*.public_ip, data.aws_instances.quorum_observer_node.*.public_ips)}"
+  value = "${length(data.aws_instance.quorum_observer_node.*.public_ip) != 0 ? coalescelist(aws_eip.quorum_observer.*.public_ip, data.aws_instances.quorum_observer_node.*.public_ips): ""}"
 }
 
 output "bootnode_ips" {
