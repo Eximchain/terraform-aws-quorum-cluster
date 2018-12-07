d80631c7aa410749fdb92324c6cfb35b3b88066a
chuacw <40189902+EximChua@users.noreply.github.com>
2018-12-07 15:03:04 +0800

    subne tinkling

--- terraform/modules/quorum-cluster-region/quorum-cluster-region-backup-event.tf
+++ terraform/modules/quorum-cluster-region/quorum-cluster-region-backup-event.tf
@@ -317,82 +317,6 @@ data "template_file" "quorum_maker_cidr_block_lambda" {
   }
 }
 
-resource "aws_subnet" "backup_lambda_public" {
-  count  = "${var.backup_enabled ? 1 : 0}"
-
-  vpc_id             = "${aws_vpc.quorum_cluster.id}"
-  availability_zone  = "${lookup(var.az_override, var.aws_region, "") == "" ? element(data.aws_availability_zones.available.names, count.index) : element(split(",", lookup(var.az_override, var.aws_region, "")), count.index)}"
-  cidr_block         = "${cidrsubnet(data.template_file.quorum_backup_lambda_public_cidr_block.rendered, 3, count.index)}"
-
-  map_public_ip_on_launch = true
-
-  tags {
-    Name      = "quorum-network-${var.network_id}-Public"
-    NodeType  = "Public-Subnet"
-    NetworkId = "${var.network_id}"
-    Region    = "${var.aws_region}"
-  }
-}
-
-resource "aws_route_table" "backup_lambda_public" {
-  count  = "${var.backup_enabled ? 1 : 0}"
-
-  vpc_id = "${aws_vpc.quorum_cluster.id}"
-
-  tags {
-     Name = "Public-${var.network_id}-${var.aws_region}-RouteTable"
-  }
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.quorum_cluster.id}"
-  }
-}
-
-resource "aws_route_table_association" "backup_lambda_public" {
-  count          = "${var.backup_enabled ? signum(lookup(var.maker_node_counts, var.aws_region, 0) + lookup(var.observer_node_counts, var.aws_region, 0) + lookup(var.validator_node_counts, var.aws_region, 0)) : 0}"
-
-  subnet_id      = "${aws_subnet.backup_lambda_public.0.id}"
-  route_table_id = "${aws_route_table.backup_lambda_public.id}" 
-}
-
-resource "aws_subnet" "backup_lambda_private" {
-  count              = "${var.backup_enabled ? signum(lookup(var.maker_node_counts, var.aws_region, 0) + lookup(var.observer_node_counts, var.aws_region, 0) + lookup(var.validator_node_counts, var.aws_region, 0)) : 0}"
-
-  vpc_id             = "${aws_vpc.quorum_cluster.id}"
-  availability_zone  = "${lookup(var.az_override, var.aws_region, "") == "" ? element(data.aws_availability_zones.available.names, count.index) : element(split(",", lookup(var.az_override, var.aws_region, "")), count.index)}"
-  cidr_block         = "${cidrsubnet(data.template_file.quorum_backup_lambda_private_cidr_block.rendered, 3, count.index)}"
-
-  tags {
-    Name      = "quorum-network-${var.network_id}-BackupLambda-NAT"
-    NodeType  = "Private-Subnet"
-    NetworkId = "${var.network_id}"
-    Region    = "${var.aws_region}"
-  }
-}
-
-resource "aws_route_table" "backup_lambda_private" {
-  count  = "${var.backup_enabled ? 1 : 0}"
-
-  vpc_id = "${aws_vpc.quorum_cluster.id}"
-
-  tags {
-     Name = "BackupLambdaSSH-${var.network_id}-${var.aws_region}-RouteTable"
-  }
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = "${aws_nat_gateway.backup_lambda.id}"
-  }
-}
-
-resource "aws_route_table_association" "backup_lambda_private" {
-  count          = "${var.backup_enabled ? 1 : 0}"
-
-  subnet_id      = "${aws_subnet.backup_lambda_private.0.id}"
-  route_table_id = "${aws_route_table.backup_lambda_private.id}" 
-}
-
 resource "aws_eip" "gateway_ip" {
   count      = "${var.backup_enabled ? signum(lookup(var.maker_node_counts, var.aws_region, 0) + lookup(var.observer_node_counts, var.aws_region, 0) + lookup(var.validator_node_counts, var.aws_region, 0)) : 0}"
 
@@ -408,12 +332,18 @@ resource "aws_eip" "gateway_ip" {
   depends_on = ["aws_internet_gateway.quorum_cluster"]
 }
 
+locals {
+  public_subnet_id = "${lookup(var.validator_node_counts, var.aws_region, 0)>0 ? aws_subnet.quorum_validator.0.id : 
+  lookup(var.maker_node_counts, var.aws_region, 0)>0 ? aws_subnet.quorum_maker.0.id :
+  lookup(var.observer_node_counts, var.aws_region, 0)> 0 ? aws_subnet.observer.0.id : ""}"
+}
+
 # NAT gateway must be in a public subnet
 resource "aws_nat_gateway" "backup_lambda" {
   count         = "${var.backup_enabled ? signum(lookup(var.maker_node_counts, var.aws_region, 0) + lookup(var.observer_node_counts, var.aws_region, 0) + lookup(var.validator_node_counts, var.aws_region, 0)) : 0}"
 
   allocation_id = "${aws_eip.gateway_ip.0.id}"
-  subnet_id     = "${aws_subnet.backup_lambda_public.0.id}" # this must be a public subnet   
+  subnet_id     = "${locals.public_subnet_id}" # this must be a public subnet   
  
   tags {
     Name      = "quorum-network-${var.network_id}-BackupLambda-NAT"
